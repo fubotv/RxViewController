1c1885ebe7669311c2caa52aad7021f16cfa90ae
Angel Casado <angel@casado.me>
2020-09-23 17:47:11 -0400

    Fixed for Xcode 12.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "RxViewController",
   platforms: [
-    .macOS(.v10_11), .iOS(.v8), .tvOS(.v9)
+    .macOS(.v10_11), .iOS(.v10), .tvOS(.v9)
   ],
   products: [
     .library(name: "RxViewController", targets: ["RxViewController"]),
